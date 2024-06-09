b996ca7086562236290f220a689e3db8882ce469
TANDO22 <uilisvandooliveiradacosta@gmail.com>
2024-06-09 12:07:53 -0400

    atualização do sistema de sorteamento

--- scripts.js
+++ scripts.js
@@ -1,7 +1,12 @@
 function cliqueinobotao() {
-    const valorminimo = Math.ceil(document.querySelector(".valor-minimo") .value)
-    const valormaximo = Math.floor(document.querySelector(".valor-maximo") .value)
+    const valorminimo = Math.ceil(document.querySelector(".valor-minimo").value)
+    const valormaximo = Math.floor(document.querySelector(".valor-maximo").value)
 
-    const resultado = Math.floor(Math.random() * (valormaximo - valorminimo + 1)) + valorminimo;
-    alert(resultado)
+    if (valorminimo >= valormaximo) {
+        alert("Altere o primeiro input para um valor menor.")
+    }
+    else {
+        const resultado = Math.floor(Math.random() * (valormaximo - valorminimo + 1)) + valorminimo;
+        alert(resultado)
+    }
 }
